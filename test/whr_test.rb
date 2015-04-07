6a7b80598de824fb3609a5133c5a6dc45af15026
trenta3 <dario.balboni.96@gmail.com>
2015-04-07 20:43:20 +0200

    Versione OK

--- test/whr_test.rb
+++ test/whr_test.rb
@@ -1,7 +1,7 @@
 require 'uowhr'
 require 'csv'
 
-@whr = UOwhr::Base.new
+@whr = UOwhr::Base.new ({:w2 => 40})
 
 sfide = CSV.read("Sfide.txt").map {|x| [Date.parse(x[0]), x[1], x[2], Float(x[3]), Float(x[4])]}
 startdate = Date.parse "2014-07-25"
@@ -10,7 +10,7 @@ playerslist = sfide.map {|x| x[1]} | sfide.map {|x| x[2]}
 
 # WholeHistoryRating::Base#create_game arguments: equal player name, opposite player name, outcome, day number
 sfide.each do |(data, giocug, giocop, puntiug, puntiop)|
-	puts "Gioco: #{giocug} vs #{giocop} - #{puntiug} a #{puntiop}, #{puntiug / (puntiug + puntiop)}, giorno #{(data - startdate).to_i}"
+	#puts "Gioco: #{giocug} vs #{giocop} - #{puntiug} a #{puntiop}, #{puntiug / (puntiug + puntiop)}, giorno #{(data - startdate).to_i}"
 	@whr.create_game(giocug, giocop, puntiug / (puntiug + puntiop), (data - startdate).to_i)
 end
 # Iterate the WHR algorithm towards convergence with more players/games, more iterations are needed.
